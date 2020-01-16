73ce0b18b1349d040a8b52c3c69ba6889822e04f
Evgeniy Denisov <lifeemulation@gmail.com>
2020-01-16 22:31:44 +0000

    ID-1519854: Fix migration

--- backend/db/migrate/20200115215729_change_holiday_expr_observed_default_to_false.rb
+++ backend/db/migrate/20200115215729_change_holiday_expr_observed_default_to_false.rb
@@ -1,5 +1,6 @@
 class ChangeHolidayExprObservedDefaultToFalse < ActiveRecord::Migration[5.2]
   def change
-    change_column :holiday_exprs, :observed, :boolean, default: false
+    HolidayExpr.where(observed: nil).update_all(observed: false)
+    change_column :holiday_exprs, :observed, :boolean, null: false, default: false
   end
 end
