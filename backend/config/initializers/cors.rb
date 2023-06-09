3049ffd13bf2cb20fa583eeeb98678f2501d07d1
Vladimir <vladimir@metroengines.jp>
2023-06-09 21:31:13 +0900

    cors corrrreeection

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /localhost:.*/, /holidays\.revenue\.metroengines\.jp/, /holidays\.metroengines\.jp/
+    origins '*'
 
-    resource '*',
+    resource '/api/*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
